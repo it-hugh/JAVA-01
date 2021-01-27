72adf680277e0c43f8e868b1f193acf7e93dad71
yomoyyy@163.com <youmeng521>
2021-01-27 23:03:55 +0800

    第三周-周三作业

---
+++ Week_03/周三-作业/nio02/src/main/java/io/github/kimmking/gateway/filter/ProxyBizFilter.java
@@ -0,0 +1,19 @@
+package io.github.kimmking.gateway.filter;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
+
+public class ProxyBizFilter implements HttpRequestFilter {
+    @Override
+    public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        String uri = fullRequest.uri();
+//        System.out.println("ProxyBizFilter接收到的请求,url: " + uri);
+        HttpHeaders headers = fullRequest.headers();
+        if (headers == null) {
+            headers = new DefaultHttpHeaders();
+        }
+        headers.add("props-add-from-request-filter", this.getClass().getSimpleName());
+    }
+}
---
+++ Week_03/周三-作业/nio02/src/main/java/io/github/kimmking/gateway/router/RandomHttpEndpointRouter.java
@@ -0,0 +1,14 @@
+package io.github.kimmking.gateway.router;
+
+import java.util.List;
+import java.util.Random;
+
+public class RandomHttpEndpointRouter implements HttpEndpointRouter {
+    @Override
+    public String route(List<String> urls) {
+        //随机转发到url列表中的某个
+        int size = urls.size();
+        Random random = new Random(System.currentTimeMillis());
+        return urls.get(random.nextInt(size));
+    }
+}
